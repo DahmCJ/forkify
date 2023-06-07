3fffe40d6f3611b573e05fdeadad746abe6fc3c0
DahmCJ <dahmcj@gmail.com>
2023-06-06 18:19:17 -0700

    Intitial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '607b7a54-5791-4c1a-b218-528585acb9b0';
+export const MODAL_CLOSE_SEC = 2.5;
